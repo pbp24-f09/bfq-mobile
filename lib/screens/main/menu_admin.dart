3063a30d2663faa8b226c8c7b90514b67e3cf363
danferdiansyah <danferdianstyle@gmail.com>
2024-11-26 19:01:23 +0700

    feat: product form with IMAGES you know.

--- lib/screens/main/menu_admin.dart
+++ lib/screens/main/menu_admin.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'menu.dart';
 import 'package:bfq/widgets/left_drawer.dart';
+import 'product_form.dart';
 
 class MenuAdminPage extends StatelessWidget {
   const MenuAdminPage({super.key});
@@ -33,11 +34,24 @@ class MenuAdminPage extends StatelessWidget {
           ),
         ],
       ),
-
       drawer: const LeftDrawer(),
-
-      body: const Center(
-        child: Text('Welcome, Admin!'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Welcome, Admin!'),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const ProductFormPage()),
+                );
+              },
+              child: const Text('Create Product'),
+            ),
+          ],
+        ),
       ),
     );
   }
